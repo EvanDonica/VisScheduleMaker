2b118f67ba9107d80dc73d9921d9f3287984bb99
Evan Donica <cevandonica@gmail.com>
2023-09-11 15:03:11 -0500

    added cors

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose.connect(`${process.env.DATABASE}`)
