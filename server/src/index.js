c1b852ed571eb142c0540b98185b88fc2aea403d
Evan Donica <cevandonica@gmail.com>
2023-09-11 09:22:20 -0500

    Server work and gitignore

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('')
+
+app.listen(3001, () => console.log('Server Running'));
+
+
